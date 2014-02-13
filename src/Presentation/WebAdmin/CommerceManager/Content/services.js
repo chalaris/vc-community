79b93b8b28e54ba426bffb99606c5d71b20d1d4d
Woland2k <sasha@virtoway.com>
2014-02-13 12:05:51 -0800

    updates

---
+++ src/Presentation/WebAdmin/CommerceManager/Content/services.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+/* Services */
+
+var vcServices = angular.module('vcServices', ['ngResource']);
+
+vcServices.factory('Shell', ['$resource',
+  function ($resource) {
+      return $resource('phones/:phoneId.json', {}, {
+          query: { method: 'GET', params: { phoneId: 'phones' }, isArray: true }
+      });
+  }]);
