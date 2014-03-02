64452f1dac6fb2ee4ff79348e54c649c004c4561
Woland2k <sasha@virtoway.com>
2014-03-01 23:21:01 -0800

    added missing files

---
+++ src/Presentation/WebAdmin/CommerceManager/Content/App/Services/route-factory.js
@@ -0,0 +1,15 @@
+﻿//Configure the routes
+VirtoApp.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider.when('/index', {
+        templateUrl: '/content/dashboard/index.html',
+        controller: 'HomeController'
+    });
+    
+    $routeProvider.when('/catalog', {
+        templateUrl: '/content/catalog/index.html',
+        controller: 'HomeController'
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/index' });
+}]);
